3527e8bc29018163fac526e3251a6fcb8df22157
Jaakko Tepponen <jaakko.tepponen@jakate.com>
2020-04-30 08:27:56 +0300

    Simple phone number validator

--- src/js/pages/FormPage.js
+++ src/js/pages/FormPage.js
@@ -67,11 +67,47 @@ const FormPage = (props) => {
     setFriendList(newList)
   }
 
+  const fixNumbers = (people) => {
+    return people.map((person) => {
+      return {
+        firstName: person.firstName,
+        phoneNumber: '+' + person.phoneNumber.replace(/\D/g, '')
+      }
+    })
+  }
+
+  const checkErrors = (numbers) => {
+    const issues = numbers.map((person) => {
+      // Number should be 10 characters long
+      if(person.phoneNumber.length !== 13) {
+        return person.firstName + "'s" + ' phone number does not look correct'
+      }
+
+      return false
+    })
+    .filter(item => item)
+
+    return issues
+  }
+
   const startTheShow = () => {
     setErrors([])
 
+    const fixedNumbers = fixNumbers(friendList)
+    const issues = checkErrors(fixedNumbers)
+    if (issues[0]) {
+      setErrors(
+        <div>
+          {issues.map((item, index) => <p key={index}>{item}</p>)}
+          <p>Example of a phone number: +358 10 123 4567</p>
+          <p>(If you want to test with a fake number, use this: +358 01 234 5678)</p>
+        </div>
+      )
+
+      return
+    }
+
     const url = 'https://europe-west3-secret-santa-bfeba.cloudfunctions.net/start'
-    //const url = 'http://localhost:5001/secret-santa-bfeba/europe-west3/start'
 
     fetch(url, {
       method: 'POST',
@@ -79,7 +115,7 @@ const FormPage = (props) => {
         ["Content-Type", "application/json"]
       ],
       body: JSON.stringify({
-        friends: friendList
+        friends: fixedNumbers
       })
     })
     .then(response => response.json())
